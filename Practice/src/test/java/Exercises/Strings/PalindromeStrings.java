f5da545f438d6f29f13ba74cb9d04996448642df
Shobana <shobanasathyanathan@gmail.com>
2022-08-30 18:40:01 +0530

    Palindrome script

---
+++ Practice/src/test/java/Exercises/Strings/PalindromeStrings.java
@@ -0,0 +1,10 @@
+package Exercises.Strings;
+
+public class PalindromeStrings {
+
+	public static void main(String[] args) {
+		System.out.println("This Program is to check for Palindrome Strings");
+		System.out.println("***********************************************");
+	}
+
+}
